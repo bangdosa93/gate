ab6a5f7a7b721c55e0c2681fbb8273c4a2b5b9fa
JiwooPark <jpark830@me.com>
2018-08-07 13:24:59 -0700

    clean directory

---
+++ assets/js/gateController.js
@@ -0,0 +1,58 @@
+let signalOn = function () {
+  let so = new XMLHttpRequest();
+  so.open("GET", "../../utils/pinon.php");
+  so.onreadystatechange = function () {
+    if (so.readyState == 4) {
+      if (so.status == 200) {
+      }
+      else {
+        alert("HTTP ERRORS");
+      }
+    }
+  }
+  so.send();
+}
+
+let signalOff = function () {
+  let so = new XMLHttpRequest();
+  so.open("GET", "../../utils/pinoff.php");
+  so.onreadystatechange = function () {
+    if (so.readyState == 4) {
+      if (so.status == 200) {
+      }
+      else {
+        alert("HTTP ERRORS");
+      }
+    }
+  }
+  so.send();
+}
+
+let alarmOff = function () {
+  let ao = new XMLHttpRequest();
+  ao.open("GET", "../../utils/alarmOff.php");
+  ao.onreadystatechange = function () {
+    if (ao.readyState == 4) {
+      if (ao.status == 200) {
+      }
+      else {
+        alert("HTTP ERRORS");
+      }
+    }
+  }
+  ao.send();
+}
+
+$(document).ready(function () {
+  $('#clickON').bind('touchstart click', function (e) {
+    signalOn();
+    $('#clickON').addClass("btn-success").removeClass("btn-danger");
+    setTimeout(function () { signalOff(); }, 300);
+    setTimeout(function () {
+      $('#clickON').prop('disabled', true);
+      $('#clickON').addClass("btn-danger").removeClass("btn-success");
+      $('#clickON').prop('disabled', false);
+      alarmOff();
+    }, 1000);
+  })
+})
---
+++ utils/pinoff.php
@@ -0,0 +1,5 @@
+<?php
+	system("gpio -g mode 14 out");
+	system("gpio -g write 14 0");
+?>
+
