601850dfb28332283f5532fef69351c96eaf221e
Jiwoo Park <jpark830@me.com>
2018-08-08 01:50:52 -0700

    uncomment out terminal commands

--- utils/gateHold-backup.php
+++ utils/gateHold-backup.php
@@ -6,10 +6,10 @@
 	system("gpio -g mode 18 out");
 	system("gpio -g write 14 1");
 	system("gpio -g write 18 1");
-	usleep(500000);
-  	system("gpio -g write 18 0");
-  	// usleep(500000);
-  	// system("gpio -g write 14 0");
+  usleep(500000);
+  system("gpio -g write 18 0");
+  // usleep(500000);
+  // system("gpio -g write 14 0");
 	
 	
 ?>
