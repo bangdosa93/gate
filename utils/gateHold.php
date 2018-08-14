fde9771197e120ca80d1e54687bfcba3dd43c244
JiwooPark <jpark830@me.com>
2018-08-14 01:21:51 -0700

    create gateHold.php

---
+++ utils/gateHold.php
@@ -0,0 +1,15 @@
+<?php
+ 	#GPIO 14 - Gate remote signal out
+	#GPIO 18 - Sound module output
+
+	system("gpio -g mode 14 out");
+	system("gpio -g mode 18 out");
+	system("gpio -g write 14 1");
+	system("gpio -g write 18 1");
+  usleep(500000);
+  system("gpio -g write 18 0");
+  // usleep(500000);
+  // system("gpio -g write 14 0");
+	
+	
+?>
