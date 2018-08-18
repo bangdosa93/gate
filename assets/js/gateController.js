ab983e9fff0c2c92bf445ca01a15036cc7adc92d
JiwooPark <jpark830@me.com>
2018-08-18 01:17:52 -0700

    add doorOpen

--- assets/js/gateController.js
+++ assets/js/gateController.js
@@ -63,6 +63,24 @@ let gateHold = function () {
   ao.send();
 }
 
+let doorOpen = function () {
+  let ao = new XMLHttpRequest();
+  ao.open("GET", "../../utils/doorOpen.php");
+  ao.onreadystatechange = function () {
+    if (ao.readyState == 4) {
+      if (ao.status == 200) {
+      }
+      else {
+        // alert("HTTP ERRORS");
+        console.log('HTTP ERROR from gateHold()')
+      }
+    }
+  }
+  ao.send();
+}
+
+
+
 $(document).ready(function () {
   // send signal to close gate every 10 seconds
   // setInterval(function(){
@@ -91,27 +109,17 @@ $(document).ready(function () {
       // $('#clickON').prop('disabled', false);
       // alarmOff();
     }, 1000);
-  // })
-  // $('#clickON').on('touchstart', function (e) {
-  //   signalOn();
-  //   $('#clickON').addClass("btn-success").removeClass("btn-danger");
-  //   setTimeout(function () { signalOff(); }, 1000);
-  //   setTimeout(function () {
-  //     // $('#clickON').prop('disabled', true);
-  //     $('#clickON').addClass("btn-danger").removeClass("btn-success");
-  //     // $('#clickON').prop('disabled', false);
-  //     alarmOff();
-  //   }, 1000);
-  // })
-  // $('#clickON').on('click', function (e) {
-  //   signalOn();
-  //   $('#clickON').addClass("btn-success").removeClass("btn-danger");
-  //   setTimeout(function () { signalOff(); }, 1000);
-  //   setTimeout(function () {
-  //     // $('#clickON').prop('disabled', true);
-  //     $('#clickON').addClass("btn-danger").removeClass("btn-success");
-  //     // $('#clickON').prop('disabled', false);
-  //     alarmOff();
-  //   }, 1000);
   })
+
+  // $('#clickON').bind('touchstart click', function (e) {
+  //   signalOn();
+  //   $('#clickON').addClass("btn-success").removeClass("btn-danger");
+  //   // setTimeout(function () { signalOff(); }, 300);
+  //   setTimeout(function () {
+  //     // $('#clickON').prop('disabled', true);
+  //     $('#clickON').addClass("btn-danger").removeClass("btn-success");
+  //     // $('#clickON').prop('disabled', false);
+  //     // alarmOff();
+  //   }, 1000);
+  // })
 })
--- utils/gateHold.php
+++ utils/gateHold.php
@@ -1,13 +1,19 @@
 <?php
  	# GPIO 4  - Gate open signal out
-        # GPIO 3  - Gate reverse signal out
-        # GPIO 2  - MAG Door signal out
-        # GPIO 14 - EMPTY RELAY
-        # GPIO 15 - Sound module signal out
+	# GPIO 3  - Gate reverse signal out
+	# GPIO 2  - MAG Door signal out
+	# GPIO 14 - EMPTY RELAY
+	# GPIO 15 - Sound module signal out
 
+	system("gpio -g mode 4 out");
 	system("gpio -g mode 3 out");
 	system("gpio -g mode 15 out");
+	
+	system("gpio -g write 4 0");
 	system("gpio -g write 3 0");
+	usleep(1000000);
+	system("gpio -g write 4 1");
+	
 	
 	for ($x = 0; $x <= 4; $x++) {
 		system("gpio -g write 15 1");
