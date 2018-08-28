2db90636d27f554b6d3ff7e8174f75e160aa601c
JiwooPark <jpark830@me.com>
2018-08-28 00:01:56 -0700

    add untracked nosleep files

---
+++ assets/js/noSleepAction.js
@@ -0,0 +1,18 @@
+let noSleep = new NoSleep;
+var wakeLockEnabled = false;
+let target = $('.no-sleep');
+console.log(target)
+target.on('touchstart', function(){
+  if (!wakeLockEnabled) {
+    noSleep.enable(); // keep the screen on!
+    wakeLockEnabled = true;
+    target.css('color',"#e0f119");
+  } else {
+    noSleep.disable(); // let the screen turn off.
+    wakeLockEnabled = false;
+    target.css('color',"white");
+  }
+})
+    
+    
+    
