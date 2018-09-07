0e716fdba72f221ae357ca8fbced5e7147444fca
JiwooPark <jpark830@me.com>
2018-09-06 18:50:50 -0700

    change button animate speed to 400

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
   const context = $('.scrolling-wrapper')
   $('.left-arrow').bind('click touchstart', function (e) {
     // console.log(context)
-    context.animate({scrollLeft: 0}, 'fast', 'swing');
+    context.animate({scrollLeft: 0}, 400, 'swing');
     
   })
   
   $('.right-arrow').bind('click touchstart', function (e) {
     // console.log(context)
-    context.animate({ scrollLeft: document.body.scrollWidth * 2}, 'fast', 'swing');
+    context.animate({ scrollLeft: document.body.scrollWidth * 2}, 400, 'swing');
     
   })
 
