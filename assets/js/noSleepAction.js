e3c2b6ad59d8cf297860e44df7516b0ed1ed6623
JiwooPark <jpark830@me.com>
2018-09-24 23:42:19 -0700

    add time schedule for door hold buttons

--- assets/js/noSleepAction.js
+++ assets/js/noSleepAction.js
@@ -1,7 +1,7 @@
 let noSleep = new NoSleep;
 var wakeLockEnabled = false;
 let target = $('.no-sleep');
-console.log(target)
+// console.log(target)
 target.bind('touchstart click', function(){
   if (!wakeLockEnabled) {
     noSleep.enable(); // keep the screen on!
