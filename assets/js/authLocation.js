3125fc1e255cd63d40ce9fbb48789f9cd10a9ee7
jpark830@me.com <jpark830@me.com>
2019-04-07 20:51:21 -0700

    remove authLocation script in index

--- assets/js/authLocation.js
+++ assets/js/authLocation.js
@@ -1,4 +1,4 @@
-$(window).on("load", function() {
+function checkLocation() {
   $(".mhn-lock-wrap").css("display", "none");
 
   const originLat = 37.7829427;
@@ -13,7 +13,7 @@ $(window).on("load", function() {
     let dist = calcDist(originLon, originLat, longitude, latitude);
     console.log("Distance: ", dist.toFixed(2), "mi.");
     $(".mhn-lock-wrap").css("display", "block");
-    if (dist > 3) {
+    if (dist > 1) {
       $(".mhn-lock").css("display", "none");
       $(".mhn-lock-title")
         .css("margin-bottom", "200px")
@@ -76,4 +76,4 @@ $(window).on("load", function() {
   } else {
     getIP();
   }
-});
+}
